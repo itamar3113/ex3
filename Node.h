222ce2a5f1b8d4a760ffa1aaf0c48b151ef8aa59
giladmev <you@example.com>
2022-12-15 00:36:32 +0200

    add base ctors

--- Node.h
+++ Node.h
@@ -1,38 +1,66 @@
 #ifndef NODE_H
 #define NODE_H
 
-template <typename T>
+template <class T>
 class Node
 {
+
+public:
+    /*
+    * C'tor of generic node with recived data and NULL as next
+    */
+    Node(T &data);
+    /*
+    * Copy c'tor of generic node 
+    */
+    Node(const Node& other);
+    //todo assignment operator
+    
+    ~Node();
+
+    T& getData() const;
+
+    Node getNext() const;
+
+    void setNext(Node next);
+
 private:
     T m_data;
     Node<T> m_next;
-
-public:
-    Node<T>(T &data):   m_data(new T(data)),
-                        m_next(NULL)
-                        { }
-    //todo should be default?
-    Node<T>(const Node<T> &) = default;
-    ~Node() = default;
-
-    template <typename T>
-    &T Node<T>::getData()
-    {
-        return &m_data;
-    }
-
-    template <typename T>
-    Node<T> Node<T>::getNext()
-    {
-        return &m_next;
-    }
-
-    template <typename T>
-    void Node<T>::setNext(Node next)
-    {
-        m_next = next;
-    }
 };
 
-#endif
+template <class T>
+Node<T>::Node(T &data):   m_data(new T(data)),
+                    m_next(nullptr)
+                    { }
+
+template <class T>
+Node<T>::Node(const Node<T> & other):
+                    m_data(new T(other.getData())),
+                    m_next(new T(other.getNext()))
+                    { }
+
+template <class T>
+Node<T>::~Node()
+{
+    delete m_data;
+}
+
+template <class T>
+T& Node<T>::getData() const
+{
+    return &m_data;
+}
+
+template <class T>
+Node<T> Node<T>::getNext() const
+{
+    return &m_next;
+}
+
+template <class T>
+void Node<T>::setNext(Node next)
+{
+    m_next = next;
+}
+#endif //NODE_H
