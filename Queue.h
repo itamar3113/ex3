e35427fde9ec473c79e56877271bb4f5c99dac65
giladmev <you@example.com>
2022-12-25 11:53:51 +0200

    encapsulation struct

--- Queue.h
+++ Queue.h
@@ -4,20 +4,23 @@
 #define EMPTY_LIST !m_head
 #define ONE_ELEMENT_LIST m_head == m_tail
 #define END_LIST !(m_current->m_next)
-template <class T>
-struct Node
-{
-	// todo encapsulation?
-	T m_data;
-	Node m_next;
-};
 
 template <class T>
 class Queue
 {
 private:
-	Node<T> m_head;
-	Node<T> m_tail;
+
+	template <class U>
+	struct Node
+	{
+		// todo encapsulation?
+		U m_data;
+		Node* m_next;
+		Node(const U& data, Node* next = nullptr): m_data(data), m_next(next) {};
+	};
+
+	Node<T>* m_head;
+	Node<T>* m_tail;
 
 public:
 	class Iterator;
@@ -36,9 +39,20 @@ public:
 	 */
 	Queue(const Queue &other);
 
-	// TODO need to change?
-	~Queue() = default;
-	// todo assignment operator
+	/*
+	* Destractor of queue
+	*/
+	~Queue();
+
+	/*
+	* delete all elements of the queue
+	*/
+	void clear();
+
+	/*
+	* Assignment operator of queue
+	*/
+	Queue& operator=(const Queue&);
 
 	/*
 	 * Add element for the end of the queue
@@ -61,6 +75,10 @@ public:
 	 * @return the size of the queue
 	 */
 	int size();
+
+	template <class Condition>
+	friend Queue<T> filter(Queue<T> &origin, Condition c);
+
 };
 
 template <class T>
@@ -70,33 +88,65 @@ Queue<T>::Queue() : m_head(nullptr),
 }
 
 template <class T>
-Queue<T>::Queue(const Queue<T> &other) : m_head(other.m_head),
-										 m_tail(other.m_tail)
+Queue<T>::Queue(const Queue<T> &other) : m_head(new T(other.m_head)),
+										 m_tail(new T(other.m_head))
 {
+	Node<T>* tmp = other.m_head;
+	while(tmp)
+	{
+		pushBack(tmp->m_data);
+		tmp = tmp->m_next;
+	}
+}
+
+template <class T>
+Queue<T>& Queue<T>::operator=(const Queue<T>& other)
+{
+	if(this == &other)
+	{
+		return *this;
+	}
+	this.clear();
+	Node<T>* tmp = other.m_head;
+	while(tmp)
+	{
+		pushBack(tmp->m_data);
+		tmp = tmp->m_next;
+	}
+	return *this;
+}
+
+template <class T>
+Queue<T>::~Queue()
+{
+	this.clear();
+}
+
+template <class T>
+void Queue<T>::clear()
+{
+	Node<T>* deleteNode = m_head;
+
+	while(deleteNode)
+	{
+		m_head = m_head->m_next;
+		delete deleteNode;
+		deleteNode = m_head;
+	}
 }
 
 template <class T>
 void Queue<T>::pushBack(const T &data)
 {
+	Node<T> insertNode = new Node<T>(data);
 	if (EMPTY_LIST)
 	{
-		m_head = new Node<T>;
-		m_head.m_data = data;
-		m_head.m_next = nullptr;
+		m_head = insertNode;
+		m_tail = insertNode;
 	}
-	else if (ONE_ELEMENT_LIST)
+	else // at least one element
 	{
-		m_tail = new Node<T>;
-		m_tail.m_data = data;
-		m_tail.m_next = nullptr;
-		m_head.m_next = m_tail;
-	}
-	else // more than one element
-	{
-		Node<T> insertNode = new Node<T>;
-		insertNode.m_data = data;
-		insertNode.m_next = nullptr;
-		m_tail.m_next = insertNode;
+		m_tail->m_next = insertNode;
 		m_tail = insertNode;
 	}
 }
@@ -122,7 +172,7 @@ void Queue<T>::popFront()
 }
 
 template <class T>
-int Queue<T>::size()
+int Queue<T>::size() //todo implement with iterator
 {
 	if (EMPTY_LIST)
 	{
@@ -152,14 +202,14 @@ template <class T, class Condition>
 Queue<T> filter(Queue<T> &origin, Condition c)
 {
 	Queue<T> filtered;
-	Node<T> tmp = origin.m_head;
-	while (tmp)
-	{ // todo encapsulation?
-		if (c(tmp.m_data))
+	Node<T>* tmp = origin.m_head;
+	while (tmp) // todo replace in for with iterator?
+	{ 
+		if (c(tmp->m_data))
 		{
-			filtered.pushBack(tmp.m_data);
+			filtered.pushBack(tmp->m_data);
 		}
-		tmp = tmp.m_next;
+		tmp = tmp->m_next;
 	}
 	return filtered;
 }
@@ -172,7 +222,7 @@ Queue<T> filter(Queue<T> &origin, Condition c)
 template <class T, class Transformator>
 void transform(Queue<T> &queue, Transformator transformator)
 {
-	Node<T> tmp = queue.m_head;
+	Node<T>* tmp = queue.m_head;
 	while (tmp)
 	{
 		tmp.m_data = transformator(tmp.m_data);
